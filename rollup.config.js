0c75b73941c97aa39a83c33ea3fb0ce8b3d0c484
gsuu <gsuu.kwak@gmail.com>
2021-11-03 15:36:53 +0900

    modify settings

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,6 @@
 import babel from '@rollup/plugin-babel';
 import resolve from '@rollup/plugin-node-resolve';
 import { uglify } from 'rollup-plugin-uglify';
-import { sizeSnapshot } from 'rollup-plugin-size-snapshot';
 
 const getBabelOptions = ({ useESModules }) => ({
   exclude: '**/node_modules/**',
@@ -19,7 +18,7 @@ export default [
     // 번들링 결과 파일과 형식(esm -> es modules, cjs -> commonjs)
     output: [{ file: 'dist/slowingDownRotation.js', format: 'esm' }],
     external: '.js',
-    plugins: [resolve(), babel(getBabelOptions({ useESModules: true })), sizeSnapshot()],
+    plugins: [resolve(), babel(getBabelOptions({ useESModules: true }))],
   },
   {
     // 번들링 기준 파일
@@ -27,6 +26,6 @@ export default [
     // 번들링 결과 파일과 형식(esm -> es modules, cjs -> commonjs)
     output: [{ file: 'dist/slowingDownRotation.min.js', format: 'esm' }],
     external: '.js',
-    plugins: [resolve(), babel(getBabelOptions({ useESModules: true })), sizeSnapshot(), uglify(),],
+    plugins: [resolve(), babel(getBabelOptions({ useESModules: true })), uglify(),],
   },
 ];
